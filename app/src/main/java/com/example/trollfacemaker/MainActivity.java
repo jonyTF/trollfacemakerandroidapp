e52efd8ab0976d172b5ee256134439fbf86101bc
Jonathan Liu <liu.z.jonathan@gmail.com>
2018-09-14 17:41:07 -0700

    Dump

---
+++ app/src/main/java/com/example/trollfacemaker/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.trollfacemaker;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSIONS_REQUEST_CAMERA = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void takePicture(View view) {
+        // Get Camera permissions
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSIONS_REQUEST_CAMERA);
+        } else {
+            Intent intent = new Intent(this, TakePictureActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_CAMERA:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission granted
+                    Intent intent = new Intent(this, TakePictureActivity.class);
+                    startActivity(intent);
+                } else {
+                    // Permission denied
+                }
+                return;
+        }
+    }
+}
